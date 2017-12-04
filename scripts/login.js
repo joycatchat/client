10dd6abe125c8dd53680f1798b650b08dae9df19
strawbee <yuejiao.hou@gmail.com>
2017-12-04 01:10:43 -0800

    basic login and register functionality working

--- scripts/login.js
+++ scripts/login.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var app = app || {};
-const __API_URL__ = 'https://joycatchat.herokuapp.com';
-// const __API_URL__ = 'http://localhost:3000';
+// const __API_URL__ = 'https://joycatchat.herokuapp.com';
+const __API_URL__ = 'http://localhost:3000';
 
 (function (module){
   const login = {};
@@ -24,8 +24,13 @@ const __API_URL__ = 'https://joycatchat.herokuapp.com';
     login.password = $('#password').val();
 
     $.post(`${__API_URL__}/register`, {'username': login.username, 'password': login.password})
-      .then(data => console.log(data))
-      .catch(err => console.error(login.password, err));
+      .then(data => {
+        if (data === 'registered') {
+          $('#validationmsg').text('Your account has been registered!');
+        }
+        else if (data === 'userexists') $('#validationmsg').text('That username already exists. Please try again.');
+      })
+      .catch(err => console.error(err));
   });
 
   module.login = login;
